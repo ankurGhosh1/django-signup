3c3b8158245148cd4b7c3bd8860abfae5c73d729
ankurGhosh1 <ankurcool.kumar@gmail.com>
2021-01-13 12:12:52 +0530

    User Extended Model Phone

--- registration/signup/models.py
+++ registration/signup/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User, auth
 
 # Create your models here.
+class UserExtended(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.phone
--- registration/signup/views.py
+++ registration/signup/views.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 import requests 
 from django.contrib.auth.models import User, auth
 from django.views.decorators.csrf import csrf_exempt
-
-
+from . models import UserExtended
+import json
 # Create your views here.
 
 @csrf_exempt
@@ -14,6 +14,7 @@ def register(request):
         firstname = request.POST['firstname']
         lastname = request.POST['lastname']
         email = request.POST['email']
+        phone = request.POST['phone']
         password = request.POST['password']
         password2 = request.POST['password2']
 
@@ -21,6 +22,11 @@ def register(request):
         user = User(username= username, first_name = firstname, last_name = lastname, email=email, password=password)
         user.set_password(password)
         user.save()
+        userExtended = UserExtended(
+            user = User.objects.get(username = username),
+            phone = phone
+        )
+        userExtended.save()
         return JsonResponse({'message': 'User Created Successfully'})
     else:
         return JsonResponse({'message': 'Password Do not Match or Password is less than 8 characters'})
@@ -32,6 +38,11 @@ def login(request):
         password = request.POST['password']
         
         user = auth.authenticate(username=username, password=password)
+        user_id = User.objects.get(username = username)
+
+        user_phone = UserExtended.objects.filter(user_id=user_id).values('phone')
+        phone = list(user_phone)
+        print(phone)
 
         if user is not None:
             auth.login(request, user)
@@ -39,7 +50,8 @@ def login(request):
                 'username': user.username,
                 'name': user.first_name,
                 'lastname': user.last_name,
-                'email': user.email
+                'email': user.email,
+                'phone': phone
             })
         else:
             return JsonResponse({'message': 'Invalid Credentials'})
